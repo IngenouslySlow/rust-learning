99cae84763b17994ea111e618ed52cbb418ddb34
IngenouslySlow <sunny8188@outlook.com>
2023-09-09 20:03:18 +0100

    48. Structs - Using Type Impl

--- section_5_rust_crash_course_intermediate/src/b_m2_structs.rs
+++ section_5_rust_crash_course_intermediate/src/b_m2_structs.rs
@@ -14,13 +14,48 @@ mod test {
 
     #[test]
     fn tests_structs() {
-        let superhero = Superhero {
+        // Structs are also mutable
+        let mut superhero = Superhero {
             name: String::from("Batman"),
             human_name: String::from("Bruce Wayne"),
             enemies_killed: 10_000,
             retired: false,
         };
+        dbg!(&superhero);
+        // Let's create a function where it can change the items in the struct
+        fn change_superhero_name(hero: &mut Superhero, new_superhero: &str) {
+            hero.name = new_superhero.to_string();
+        }
 
-        dbg!(superhero);
+        change_superhero_name(&mut superhero, "Redhood");
+
+        dbg!(&superhero);
+
+        // You can also create so many functions using impl referencing the struct itself using the world self
+        impl Superhero {
+            fn increment_enemies(&mut self) {
+                self.enemies_killed += 1;
+            }
+
+            fn change_name(&mut self, new_name: &str) {
+                self.name = new_name.to_string();
+            }
+
+            fn change_human_name(&mut self, new_name: &str) {
+                self.human_name = new_name.to_string();
+            }
+
+            fn change_retired(&mut self) {
+                self.retired = !self.retired;
+            }
+        }
+
+        // Now you can use them as follows
+
+        superhero.change_human_name("Clark kent");
+        superhero.change_name("Superman");
+        superhero.change_retired();
+        superhero.increment_enemies();
+        dbg!(&superhero);
     }
 }
--- section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5.d
+++ section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5.d
@@ -1,5 +1,7 @@
-/Users/sunny/Desktop/Docs/Learn/rust-learning/section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5: src/main.rs
+/Users/sunny/Desktop/Docs/Learn/rust-learning/section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5: src/main.rs src/a_m1_enums.rs src/b_m2_structs.rs
 
-/Users/sunny/Desktop/Docs/Learn/rust-learning/section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5.d: src/main.rs
+/Users/sunny/Desktop/Docs/Learn/rust-learning/section_5_rust_crash_course_intermediate/target/debug/deps/section_5_rust_crash_course_intermediate-53325bd8705e64c5.d: src/main.rs src/a_m1_enums.rs src/b_m2_structs.rs
 
 src/main.rs:
+src/a_m1_enums.rs:
+src/b_m2_structs.rs:
